0786192e1d06b8a68af0e08d1b86abe18b320a13
whispernorbury <jsw777777@gmail.com>
2024-04-04 13:30:07 +0900

    try install

--- api/install.php
+++ api/install.php
@@ -4,17 +4,18 @@
   if ($_SERVER["REQUEST_METHOD"]=="POST") {
     $username=$_POST["username"];
     $host=$_POST["host"];
-    $pemkey=$_POST["sshkey.pem"];
+    $pemkey=$_FILES["sshkey.pem"]["tmp_name"];
     $repo="https://github.com/whispernorbury/comkaraka-back.git";
 
     # Parse .env
-    $envfile=$_POST["envfile"];
-    $envals=parse_ini_file($envfile);
+    $envfile=$_FILES["envfile"]["tmp_name"];
+    $envs=file_get_contents($envfile);
+    $envals=parse_ini_file($envs);
     $MYSQL_ROOT_PASSWORD=$envals["MYSQL_ROOT_PASSWORD"];
     $ELASTIC_PASSWORD=$envals["ELASTIC_PASSWORD"];
 
     # install
-    $scritp_path="./installscript.sh";
+    $scritp_path="installscript.sh";
     $handle = popen("sh $script_path $username $host $pemkey $repo $MYSQL_ROOT_PASSWORD $ELASTIC_PASSWORD 2>&1", "r");
 
     while (!feof($handle)) {
