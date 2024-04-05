aa8f3d7db980afaad72c1b58682fa281faf15e63
whispernorbury <jsw777777@gmail.com>
2024-04-06 00:21:19 +0900

    try ssh

--- sh/migrate.sh
+++ sh/migrate.sh
@@ -11,13 +11,13 @@ new_pemkey=$6
 tarName="vol_$(date +%Y-%m-%d_%H-%M-%S).tar.gz"
 
 # Migrate
-ssh -i $old_pemkey $old_username@$old_host "
+/usr/bin/ssh -i $old_pemkey $old_username@$old_host "
 sudo tar -zcvf $tarName -P /root/Docker/comkaraka;
 scp -i $new_pemkey ~/$tarName $new_username@$new_host:~
 "
 
 # Install volumes
-ssh -i $new_pemkey $new_username@$new_host "
+/usr/bin/ssh -i $new_pemkey $new_username@$new_host "
 sudo mkdir /root/Docker;
 sudo tar -zxvf $tarName -C /root/Docker;
 sudo chmod -R g+w /root/Docker/comkaraka;
