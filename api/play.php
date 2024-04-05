0c3ea8a772d80d22011fed5c4c69d9469fc25dfb
whispernorbury <jsw777777@gmail.com>
2024-04-05 17:31:59 +0900

    ob

--- api/play.php
+++ api/play.php
@@ -1,8 +1,13 @@
 <?php
-  ob_end_clean();
+  @ini_set('zlib.output_compression', 0);
+  @ini_set('implicit_flush', 1);
+  @ob_end_clean();
+  ob_implicit_flush(true);
+
   header("Content-Type: application/json");
 
   if ($_SERVER["REQUEST_METHOD"]==="GET") {
+    ob_start();
     $script_path="/var/task/user/sh/play.sh";
     $command="sh $script_path";
     $process = popen("$command 2>&1", "r");
@@ -10,8 +15,8 @@
       while (!feof($process)) {
         $output=fread($process, 8192);
         echo $output;
-        ob_flush();
         flush();
+        ob_flush();
       }
       $exitCode=pclose($process);
       echo "Exit: $exitCode";
