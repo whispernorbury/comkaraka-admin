dec532ee4d6a467f054b77d8e65887758f9dbcd6
whispernorbury <jsw777777@gmail.com>
2024-04-05 16:10:14 +0900

    popen

--- api/play.php
+++ api/play.php
@@ -4,25 +4,14 @@
   if ($_SERVER["REQUEST_METHOD"]==="GET") {
     $script_path="/var/task/user/sh/play.sh";
     $command="sh $script_path";
-    $descriptors= [
-    ];
-    $process = proc_open($command, array(
-      0 => ["pipe", "r"],
-      1 => ["pipe", "w"],
-      2 => ["pipe", "w"]
-    ), $pipes);
-    if (is_resource($process)) {
-      while (($stdout = fgets($pipes[1])) || ($stderr = fgets($pipes[2]))) {
-        if (!empty($stdout)) {
-          echo $stdout;
-          flush();
-        }
-        if (!empty($stderr)) {
-          echo $stderr;
-          flush();
-        }
+    $process = popen("$command 2>&1", "r");
+    if ($process) {
+      while (!feof($process)) {
+        $output=fread($process, 8192);
+        echo $output;
+        flush();
       }
-      $exitCode=proc_close($process);
+      $exitCode=pclose($process);
       echo "Exit: $exitCode";
     }
   }
