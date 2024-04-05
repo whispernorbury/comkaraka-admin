9ce88948db9ae31586044cfc8cba08fded18318c
whispernorbury <jsw777777@gmail.com>
2024-04-05 15:25:31 +0900

    Check sh dir

--- api/install.php
+++ api/install.php
@@ -21,30 +21,32 @@
 
     # install
     $script_path="/sh/install";
-    $command="sh $script_path $username $host $repo $pemkey $envfile $firebase_key $MYSQL_ROOT_PASSWORD $ELASTIC_PASSWORD";
-    $descriptors= [
-      1 => ["pipe", "w"],
-      2 => ["pipe", "w"],
-    ];
-    $process = proc_open($command, $descriptors, $pipes);
-    if (is_resource($process)) {
-      stream_set_blocking($pipes[1], false);
-      stream_set_blocking($pipes[2], false);
-      while (($stdout = fgets($pipes[1])) || ($stderr = fgets($pipes[2]))) {
-        if (!empty($stdout)) {
-          echo $stdout;
-          flush();
-        }
-        if (!empty($stderr)) {
-          echo $stderr;
-          flush();
-        }
-        usleep(100000);
-      }
-      fclose($pipes[1]);
-      fclose($pipes[2]);
-      $exitCode=proc_close($process);
-      echo "Exit: $exitCode";
-    }
+    echo file_get_contents($script_path);
+
+    // $command="sh $script_path $username $host $repo $pemkey $envfile $firebase_key $MYSQL_ROOT_PASSWORD $ELASTIC_PASSWORD";
+    // $descriptors= [
+    //   1 => ["pipe", "w"],
+    //   2 => ["pipe", "w"],
+    // ];
+    // $process = proc_open($command, $descriptors, $pipes);
+    // if (is_resource($process)) {
+    //   stream_set_blocking($pipes[1], false);
+    //   stream_set_blocking($pipes[2], false);
+    //   while (($stdout = fgets($pipes[1])) || ($stderr = fgets($pipes[2]))) {
+    //     if (!empty($stdout)) {
+    //       echo $stdout;
+    //       flush();
+    //     }
+    //     if (!empty($stderr)) {
+    //       echo $stderr;
+    //       flush();
+    //     }
+    //     usleep(100000);
+    //   }
+    //   fclose($pipes[1]);
+    //   fclose($pipes[2]);
+    //   $exitCode=proc_close($process);
+    //   echo "Exit: $exitCode";
+    // }
   }
 ?>
