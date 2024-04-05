4a99c503c52d73418f31acc7877c0fc7b638a3da
whispernorbury <jsw777777@gmail.com>
2024-04-05 17:34:25 +0900

    ab

--- api/install.php
+++ api/install.php
@@ -23,29 +23,5 @@
     $script_path="/var/task/user/sh/install.sh";
 
     $command="sh $script_path $username $host $repo $pemkey $envfile $firebase_key $MYSQL_ROOT_PASSWORD $ELASTIC_PASSWORD";
-    $descriptors= [
-      1 => ["pipe", "w"],
-      2 => ["pipe", "w"],
-    ];
-    $process = proc_open($command, $descriptors, $pipes);
-    if (is_resource($process)) {
-      stream_set_blocking($pipes[1], false);
-      stream_set_blocking($pipes[2], false);
-      while (($stdout = fgets($pipes[1])) || ($stderr = fgets($pipes[2]))) {
-        if (!empty($stdout)) {
-          echo $stdout;
-          flush();
-        }
-        if (!empty($stderr)) {
-          echo $stderr;
-          flush();
-        }
-        usleep(100000);
-      }
-      fclose($pipes[1]);
-      fclose($pipes[2]);
-      $exitCode=proc_close($process);
-      echo "Exit: $exitCode";
-    }
   }
 ?>
