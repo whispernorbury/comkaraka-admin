af99d8919692a106a5eda2893f37db331b8b0162
whispernorbury <jsw777777@gmail.com>
2024-04-05 15:20:50 +0900

    gpt

--- api/install.php
+++ api/install.php
@@ -21,14 +21,29 @@
 
     # install
     $script_path="/sh/install";
-    $handle = popen("sh $script_path $username $host $repo $pemkey $envfile $firebase_key $MYSQL_ROOT_PASSWORD $ELASTIC_PASSWORD 2>&1", "r");
-
-    while (!feof($handle)) {
-      $output=fgets($handle);
-
-      flush();
-      ob_flush();
+    $command="sh $script_path $username $host $repo $pemkey $envfile $firebase_key $MYSQL_ROOT_PASSWORD $ELASTIC PASSWORD";
+    $descriptors= [
+      1 => ["pipe", "w"],
+      2 => ["pipe", "w"],
+    ];
+    $process = proc_open($command, $descriptors, $pipes);
+    if (is_resource($process)) {
+      stream_set_blocking($pipes[1], false);
+      stream_set_blocking($pipes[2], false);
+      while (($stdout = fgets($pipes[1])) || ($stderr = fgets($pipes[2]))) {
+        if (!empty($stdout)) {
+          echo $stdout;
+          flush();
+        }
+        if (!empty($stderr)) {
+          echo $stderr;
+          flush();
+        }
+      }
+      fclose($pipes[1]);
+      fclose($pipes[2]);
+      $exitCode=proc_close($process);
+      echo "Exit: $exitCode";
     }
-    pclose($handle);
   }
 ?>
