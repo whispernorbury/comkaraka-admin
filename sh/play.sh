7242142f873beac6914f18209e7f0d5f888cff01
whispernorbury <jsw777777@gmail.com>
2024-04-05 17:41:37 +0900

    Play More

--- sh/play.sh
+++ sh/play.sh
@@ -1,9 +1,19 @@
 #!bin/bash
 
-echo HI
+echo "HI"
 sleep 0.5
 
-echo "I will wait you 3s"
+echo "Good job! You found easter egg!"
+sleep 1
+
+echo "Or.... You checked repository? XD"
+sleep 0.5
+
+echo "Anyway,"
+sleep 0.5
+
+echo "I will wait you 3s for test"
 sleep 3
 
 echo "waited 3s"
+echo "#### script END #### "
