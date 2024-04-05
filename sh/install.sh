a6483579dd030d8091b5a793fea29baa09879fc2
whispernorbury <jsw777777@gmail.com>
2024-04-06 02:22:54 +0900

    ssh install

--- sh/install.sh
+++ sh/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Configuration
+sudo apt-get install openssh-client
 username=$1
 host=$2
 repo=$3
@@ -11,29 +12,29 @@ MYSQL_ROOT_PASSWORD=$7
 ELASTIC_PASSWORD=$8
 
 # refresh ssh key
-/usr/bin/ssh -i $pemkey $username@$host exit
+ssh -i $pemkey $username@$host exit
 
 if [ $? -eq 0 ]; then
 
   # install Docker(compose)
-  /usr/bin/ssh -i $pemkey $username@$host "
+  ssh -i $pemkey $username@$host "
   sudo apt-get update;
   curl -fsSL https://get.docker.com -o docker-install.sh;
   sudo sh docker-install.sh;
   "
 
   # clone repo and modify environmentals
-  /usr/bin/ssh -i $pemkey $username@$host "
+  ssh -i $pemkey $username@$host "
   git clone $repo;
   sed -i 's/\${MYSQL_ROOT_PASSWORD}/$MYSQL_ROOT_PASSWORD/g' comkaraka-back/elk/logstash/config/*.conf;
   sed -i 's/\${ELASTIC_PASSWORD}/$ELASTIC_PASSWORD/g' comkaraka-back/elk/logstash/config/*.conf;
   "
 
   # copy private files
-  /usr/bin/scp -i $pemkey $envfile $firebase_key $username@$host:~/comkaraka-back
+  scp -i $pemkey $envfile $firebase_key $username@$host:~/comkaraka-back
 
   # permission setting
-  /usr/bin/ssh -i $pemkey $username@$host "
+  ssh -i $pemkey $username@$host "
   sudo docker compose up --build -d;
   sudo docker compose down;
   sudo chmod -R g+w /root/Docker/comkaraka;
